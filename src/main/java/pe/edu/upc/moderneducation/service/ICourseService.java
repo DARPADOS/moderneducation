8611cd4fe6dd875b323b474e8253830346a2ceb5
Joseph <josephgamboasanchez@gmail.com>
2021-05-02 01:24:16 -0500

    Primer avance Course CRUD

--- src/main/java/pe/edu/upc/moderneducation/service/ICourseService.java
+++ src/main/java/pe/edu/upc/moderneducation/service/ICourseService.java
@@ -1,5 +1,12 @@
 package pe.edu.upc.moderneducation.service;
 
-public interface ICourseService {
+import java.util.List;
 
+import pe.edu.upc.moderneducation.models.entities.Course;
+
+public interface ICourseService {
+	public void insert(Course co, Integer idTeacher);
+	List<Course> list();
+	public void delete(int idCourse);
+	public Integer update(Course co);
 }
--- src/main/java/pe/edu/upc/moderneducation/serviceimpl/CourseServiceImpl.java
+++ src/main/java/pe/edu/upc/moderneducation/serviceimpl/CourseServiceImpl.java
@@ -1,5 +1,44 @@
 package pe.edu.upc.moderneducation.serviceimpl;
 
-public class CourseServiceImpl {
+import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import pe.edu.upc.moderneducation.dao.ICourseDao;
+import pe.edu.upc.moderneducation.models.entities.Course;
+import pe.edu.upc.moderneducation.service.ICourseService;
+
+@Named
+@RequestScoped
+public class CourseServiceImpl implements ICourseService{
+	@Inject
+	private ICourseDao cDao;
+
+	@Override
+	public void insert(Course co, Integer idTeacher) {
+		// TODO Auto-generated method stub
+		cDao.insert(co, idTeacher);
+	}
+
+	@Override
+	public List<Course> list() {
+		// TODO Auto-generated method stub
+		return cDao.list();
+	}
+
+	@Override
+	public void delete(int idCourse) {
+		// TODO Auto-generated method stub
+		cDao.delete(idCourse);
+		
+	}
+
+	@Override
+	public Integer update(Course co) {
+		// TODO Auto-generated method stub
+		return cDao.update(co);
+	}
+	
 }
