621a0785489318ddec336b15fdf94e23e4f5da3a
Valeria Barraza <barrazavargasxiomara@gmail.com>
2021-05-02 22:35:40 -0500

    se creo la clase ChapterController y metodos especializados

---
+++ src/main/java/pe/edu/upc/moderneducation/controller/ChapterController.java
@@ -0,0 +1,77 @@
+package pe.edu.upc.moderneducation.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import pe.edu.upc.moderneducation.models.entities.Chapter;
+import pe.edu.upc.moderneducation.service.IChapterService;
+
+@Named
+public class ChapterController {
+	@Inject
+	private IChapterService rService; 
+	
+	//variables
+	private Chapter chapter;
+	List<Chapter>listaChapter;
+	
+	//constructores
+	@PostConstruct 
+	public void init() {
+		this.chapter=new Chapter();
+		this.listaChapter=new ArrayList<Chapter>();	
+		this.list();
+	}
+	//metodos especializados
+	public String newChapter() {
+		this.setChapter(new Chapter());
+		return "Chapter.xhtml";
+	}
+	public void insert() {
+		try {
+			rService.insert(chapter);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error al insertar en el controller el chapter");
+		}
+	}
+	public void list() {
+		try {
+			listaChapter=rService.list();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error al listar en el controller de chapter");
+		}
+	}
+	public void delet(Chapter re) {
+		try {
+			rService.delete(re.getId());
+			this.list();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error al eliminar en el controller de chapter");
+		}
+	}
+	
+	//get y set
+	public Chapter getChapter() {
+		return chapter;
+	}
+	public void setChapter(Chapter chapter) {
+		this.chapter = chapter;
+	}
+	public List<Chapter> getListaChapter() {
+		return listaChapter;
+	}
+	public void setListaChapter(List<Chapter> listaChapter) {
+		this.listaChapter = listaChapter;
+	}
+	
+	
+	
+}
